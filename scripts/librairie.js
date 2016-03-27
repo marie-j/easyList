32c7bfbfca22b13fc39b880bb35594659c162edd
marie <marie.jones@etudiant.univ-lille1.fr>
2016-03-27 17:06:07 +0200

    affichage/suppression recette

--- scripts/librairie.js
+++ scripts/librairie.js
@@ -1,7 +1,7 @@
-var addingredient = `<form class="ingredient">
-                    <input type="text" placeholder="Ingredient" id="nom">
-                    <input type="number" placeholder="Quantite" id="quantite">
-                    <select id="unite">
+var addingredient = `<form>
+                    <input type="text" placeholder="Ingredient" class="nom">
+                    <input type="number" placeholder="Quantite" class="quantite">
+                    <select class="unite">
                       <option selected> Unite(s) </option>
                       <option>Cuillère(s) à soupe</option>
                       <option>Cuillère(s) à café</option>
@@ -9,3 +9,27 @@ var addingredient = `<form class="ingredient">
                       <option>Gramme(s)</option>
                     </select>
                     </form>`;
+
+var titreRecette = function(e,n) {
+  return `<div id="` + n + `">
+            <p>` + e + `</p>
+            <button type="button" class="edit" id="` + n + `"> Modifier la recette </button>
+            <button type="button" class="delete" id="` + n + `"> Supprimer la recette</button>
+          </div>`
+}
+
+var afficherIngredients = function(e,db,elem) {
+  var ingredients = db.get(e.id).ingredients;
+  elem.append(`<button type="button" class="addIngredient" id="` + e.id  + `"> Ajouter un ingrédient </button>`)
+  for (var i = 0 ; i <ingredients.length ; i ++) {
+    elem.append(`<div class="ingredient" id= "` + i + `">
+                  <p>` + ingredients[i].ingredient + " " + ingredients[i].quantite + " " + ingredients[i].unite + `</p>
+                  <button type="button" class="editIngredient" id="` + i + `">Modfier l'ingrédient </button>
+                  <button type="button" class="deleteIngredient" id="` + i + `">Supprimer l'ingrédient</button>
+                  </div>`);
+  }
+}
+
+var supprRecette = function(e, db) {
+  db.remove(e.id);
+}
