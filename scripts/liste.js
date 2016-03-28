8b58214b5a0931c362e1c1ae6534612788b05940
marie <marie.jones@etudiant.univ-lille1.fr>
2016-03-28 18:08:55 +0200

    reglage bouton ok

--- scripts/liste.js
+++ scripts/liste.js
@@ -8,12 +8,14 @@ $(document).ready(function(){
     $('.liste').empty();
     var tmp = liste.all();
     for (var i = 0 ; i< tmp.length; i++) {
-      $('.liste').append(listeCourse(tmp[i].nom,i));
+      $('.liste').append(listeCourse(tmp[i],i));
     }
 
     $('.check').click(function(){
       var elem = $('div#' + this.id + '>p');
       elem.replaceWith('<span style="text-decoration:line-through;">'+ elem.text() +'</span></br>');
+      this.remove();
+
     });
 
     $('.delete').click(function(){
@@ -36,7 +38,9 @@ $(document).ready(function(){
 
     if (r == "") {
       var article = {
-        nom: $('#article').val()
+        nom: $('#article').val(),
+        quantite: "",
+        unite : ""
       }
       liste.post(article);
     }
@@ -47,11 +51,26 @@ $(document).ready(function(){
         window.alert("Cette recette n'est pas dans votre liste !");
       }
       else {
+
+        var nb = $('#nombre').val();
+        if (nb == "") {
+          nb = recette.personnes;
+        }
+
+        if (recette.personnes == nb) {
+          var coef = 1;
+        }
+        else {
+          var coef = nb/recette.personnes;
+        }
+
         var tab = recette.ingredients;
         for (var i = 0 ; i < tab.length; i++ ) {
-          console.log(tab[i]);
+          var q = tab[i].quantite * coef
           var article = {
-            nom : tab[i].ingredient
+            nom : tab[i].ingredient,
+            quantite : q,
+            unite: tab[i].unite
           }
           liste.post(article);
         }
@@ -60,6 +79,7 @@ $(document).ready(function(){
 
 
     $('#titre').val("");
+    $('#nombre').val("");
     $('#article').val("");
     update();
   });
