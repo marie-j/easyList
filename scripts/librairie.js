f0b3931015cac9135f66bb06bc76ff3033f4c27e
marie <marie.jones@etudiant.univ-lille1.fr>
2016-03-28 14:32:04 +0200

    ajout boutons

--- scripts/librairie.js
+++ scripts/librairie.js
@@ -14,6 +14,7 @@ var titreRecette = function(e,n) {
   return `<div id="` + n + `">
           <section>
             <p>` + e + `</p>
+            <button type="button" class="show" id="` + n + `"> Voir la recette </button>
             <button type="button" class="edit" id="` + n + `"> Modifier la recette </button>
             <button type="button" class="delete" id="` + n + `"> Supprimer la recette</button>
           </section>
@@ -24,9 +25,15 @@ var enregistrer = `<button type="button" class="save">Enregister</button>`;
 
 var annuler = `<button type="button" class="cancel">Annuler</button>`;
 
+var retourAuMenu = `<button type="button" class="back">Retour au menu</button>`;
+
+var modifier = function(i) {
+  return `<button type="button" class="deleteIngredient" id="` + i + `"> Modifier l'ingrédient</button>`;
+}
+
 var afficherIngredients = function(e,db,elem) {
   var ingredients = db.get(e.id).ingredients;
-  elem.append(`<button type="button" class="addIngredient" id="` + e.id  + `"> Ajouter un ingrédient </button>`)
+  elem.append(`<button type="button" class="addIngredient" id="` + e.id  + `"> Ajouter un ingrédient </button>`);
   for (var i = 0 ; i <ingredients.length ; i ++) {
     elem.append(`<div class="ingredient" id= "` + i + `">
                   <p>` + ingredients[i].ingredient + " " + ingredients[i].quantite + " " + ingredients[i].unite + `</p>
@@ -34,6 +41,16 @@ var afficherIngredients = function(e,db,elem) {
                   <button type="button" class="deleteIngredient" id="` + i + `">Supprimer l'ingrédient</button>
                   </div>`);
   }
+};
+
+var show = function(e,db,elem) {
+  var ingredients = db.get(e.id).ingredients;
+  elem.append(`<button type="button" class="addIngredient" id="` + e.id  + `"> Ajouter un ingrédient </button>`);
+  for (var i = 0 ; i <ingredients.length ; i ++) {
+    elem.append(`<div class="ingredient" id= "` + i + `">
+                  <p>` + ingredients[i].ingredient + " " + ingredients[i].quantite + " " + ingredients[i].unite + `</p>
+                  </div>`);
+}
 };
 
 var suppr = function(e, db) {
@@ -59,4 +76,28 @@ var ajouterAuTableau= function(tab,ingredients,quantite,unite) {
   }
   return tab;
 
+}
+
+var modifIngredient = function(tab,indice) {
+  var ingredient = tab[indice];
+  var nom = ingredient.ingredient;
+  var quantite = ingredient.quantite;
+  var unite = ingredient.unite;
+  return `<form>
+          <input type="text" value ="` + nom + `" id="nom">
+          <input type="number" value ="` + quantite +`" id = "quantite">
+          <select value="` + unite + `" id="unite">
+            <option> Unite(s) </option>
+            <option>Cuillère(s) à soupe</option>
+            <option>Cuillère(s) à café</option>
+            <option>Litre(s)</option>
+            <option>Gramme(s)</option>
+          </select>
+          </form>
+          <button type="button" class="valid" id="` + indice + `"> Valider </button>`
+          ;
+};
+
+var afficheModif = function(ingred,qtte,unite) {
+  return `<p>` + ingred + " " + qtte + " " + unite + `</p>`;
 }
